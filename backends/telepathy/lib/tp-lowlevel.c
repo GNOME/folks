aa5929f9aa4c7016ac79b140bd2dbd6d23b3fa50
Travis Reitter <travis.reitter@collabora.co.uk>
2010-09-16 10:36:30 -0700

    Cut some obsolete comments in TpLowlevel.

--- backends/telepathy/lib/tp-lowlevel.c
+++ backends/telepathy/lib/tp-lowlevel.c
@@ -95,9 +95,6 @@ folks_tp_lowlevel_connection_open_contact_list_channel_async (
       connection_ensure_channel_cb, result, NULL, G_OBJECT (conn));
 }
 
-/* XXX: ideally, we'd either make this static or hide it in the .metadata file,
- * but neither seems to be supported (without breaking the binding to the async
- * function) */
 /**
  * folks_tp_lowlevel_connection_open_contact_list_channel_finish:
  * @lowlevel: a #FolksTpLowlevel
@@ -205,9 +202,6 @@ folks_tp_lowlevel_connection_get_contacts_by_handle_async (
       G_OBJECT (conn));
 }
 
-/* XXX: ideally, we'd either make this static or hide it in the .metadata file,
- * but neither seems to be supported (without breaking the binding to the async
- * function) */
 /**
  * folks_tp_lowlevel_connection_get_contacts_by_handle_finish:
  * @tp_lowlevel: a #FolksTpLowlevel
@@ -317,9 +311,6 @@ folks_tp_lowlevel_connection_get_contacts_by_id_async (
       G_OBJECT (conn));
 }
 
-/* XXX: ideally, we'd either make this static or hide it in the .metadata file,
- * but neither seems to be supported (without breaking the binding to the async
- * function) */
 /**
  * folks_tp_lowlevel_connection_get_contacts_by_id_finish:
  * @tp_lowlevel: a #FolksTpLowlevel
