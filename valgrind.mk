e3458da97be361570872f591b42e8bc51e847374
Philip Withnall <philip@tecnocode.co.uk>
2013-01-03 11:04:11 +0000

    build: Rewrite valgrind.mk to use GNU Make conditionals
    This means we’re not overwriting rules and potentially changing their
    prerequisites. Incorrectly overwriting check-* rules was causing tests to
    be run twice in the tests/folks directory.
    
    See: https://bugzilla.gnome.org/show_bug.cgi?id=690990

--- valgrind.mk
+++ valgrind.mk
@@ -1,18 +1,15 @@
-check: $(BUILT_SOURCES)
-	if test -n "$$FOLKS_TEST_VALGRIND"; then \
-		G_DEBUG=${G_DEBUG:+"${G_DEBUG},"}gc-friendly; \
-		G_SLICE=${G_SLICE},always-malloc; \
-		$(MAKE) $(AM_MAKEFLAGS) \
-			TESTS_ENVIRONMENT="$(TESTS_ENVIRONMENT) \
-			libtool --mode=execute valgrind \
-					--leak-check=full \
-					--show-reachable=no \
-					--gen-suppressions=all \
-					--num-callers=20 \
-					--error-exitcode=0 \
-					--log-file=valgrind.log.%p" \
-			check-am; \
-	else \
-		$(MAKE) $(AM_MAKEFLAGS) check-am; \
-	fi
-
+# Pass FOLKS_TEST_VALGRIND=1 to make to enable Valgrind on the tests
+# This file must be included _after_ TESTS_ENVIRONMENT has been set by the Makefile.am.
+ifeq ($(FOLKS_TEST_VALGRIND),1)
+TESTS_ENVIRONMENT := \
+	G_DEBUG=$(G_DEBUG),gc-friendly \
+	G_SLICE=$(G_SLICE),always-malloc \
+	$(TESTS_ENVIRONMENT) \
+	$(LIBTOOL) --mode=execute valgrind \
+		--leak-check=full \
+		--show-reachable=no \
+		--gen-suppressions=all \
+		--num-callers=20 \
+		--error-exitcode=0 \
+		--log-file=valgrind.log.%p
+endif
