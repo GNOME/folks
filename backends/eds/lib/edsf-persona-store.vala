GNOME/folks

commit dcc558d87d113dae0aa8721db3d80261c08dee57

eds: fix Android favourite handling when creating contact
When processing PersonaDetail.GROUPS before PersonaDetail.IS_FAVOURITE
while creating a new contact in EDS, the special code for adding the
contact to the “Starred in Android” group (bgo#661490 was skipped). We
must go through _set_is_favourite instead of _set_contact_is_favourite
to get that part done, too.

backends/eds/lib/edsf-persona-store.vala
@@ -547,7 +547,9 @@ public class Edsf.PersonaStore : Folks.PersonaStore
                   PersonaDetail.IS_FAVOURITE))
             {
               is_fav = v.get_boolean ();
-              this._set_contact_is_favourite (contact, is_fav);
+              // Use _set_is_favourite here to get the contact added
+              // to the Android "favourite" group.
+              this._set_is_favourite (contact, is_fav);
             }
         }
 

commit 26310e79259dda6ee795361fb3d7164ea2d10eb3

eds: Fix the type of a parameter in add_persona_from_details()
This build breaker was introduced in commit
dcc558d87d113dae0aa8721db3d80261c08dee57.

backends/eds/lib/edsf-persona-store.vala
@@ -416,6 +416,7 @@ public class Edsf.PersonaStore : Folks.PersonaStore
       unowned string k;
       unowned Value? _v;
       bool is_fav = false; // Remember this for _set_contact_groups.
+      Set<string> groups = new HashSet<string> (); // For _set_is_favourite
 
       while (iter.next (out k, out _v) == true)
         {
@@ -539,17 +540,17 @@ public class Edsf.PersonaStore : Folks.PersonaStore
             }
           else if (k == Folks.PersonaStore.detail_key (PersonaDetail.GROUPS))
             {
-              Set<string> groups =
-                (Set<string>) v.get_object ();
+              groups = (Set<string>) v.get_object ();
               this._set_contact_groups (contact, groups, is_fav);
             }
           else if (k == Folks.PersonaStore.detail_key (
                   PersonaDetail.IS_FAVOURITE))
             {
               is_fav = v.get_boolean ();
-              // Use _set_is_favourite here to get the contact added
-              // to the Android "favourite" group.
-              this._set_is_favourite (contact, is_fav);
+              this._set_contact_is_favourite (contact, is_fav);
+              /* Ensure the contact is added to the “Starred in Android” group
+               * if appropriate. */
+              this._set_contact_groups (contact, groups, is_fav);
             }
         }
 