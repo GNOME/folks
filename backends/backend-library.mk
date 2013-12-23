GNOME/folks

commit 150858a0237291d8269d222defcb82f54c1125cf

build: Ensure backends correctly link to internal folks libraries

backends/backend-library.mk
@@ -113,6 +113,7 @@ backend_library_cflags = $(backend_library_cflags_generic)
 backend_library_libadd_generic = \
 	$(AM_LIBADD) \
 	$(top_builddir)/folks/libfolks.la \
+	$(top_builddir)/folks/libfolks-internal.la \
 	$(GIO_LIBS) \
 	$(GLIB_LIBS) \
 	$(GEE_LIBS) \

backends/backend.mk
@@ -38,6 +38,7 @@ backend_valaflags = \
 	--vapidir=$(top_builddir)/folks \
 	--pkg folks \
 	--pkg folks-internal \
+	--pkg folks-generics \
 	--pkg gee-0.8 \
 	--pkg gio-2.0 \
 	--pkg gobject-2.0 \

commit ef76b7cacb5cfdf7963e5c2a2fe3bc8c1c97838e

build: Only link the "endpoint" backend libraries against libfolks-internal
This fixes a build-breaking bug introduced in commit 150858a023

backends/backend-library.mk
@@ -113,12 +113,14 @@ backend_library_cflags = $(backend_library_cflags_generic)
 backend_library_libadd_generic = \
 	$(AM_LIBADD) \
 	$(top_builddir)/folks/libfolks.la \
-	$(top_builddir)/folks/libfolks-internal.la \
 	$(GIO_LIBS) \
 	$(GLIB_LIBS) \
 	$(GEE_LIBS) \
 	$(NULL)
-backend_library_libadd = $(backend_library_libadd_generic)
+backend_library_libadd = \
+	$(backend_library_libadd_generic) \
+	$(top_builddir)/folks/libfolks-internal.la \
+	$(NULL)
 
 backend_library_ldflags_generic = \
 	$(AM_LDFLAGS) \